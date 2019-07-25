69bda974be093f2cd89ca51782ca9b64ad618215
mwalliczek <35632862+mwalliczek@users.noreply.github.com>
2019-07-25 17:51:05 +0200

    Update createTestDb.sh

--- createTestDb.sh
+++ createTestDb.sh
@@ -9,5 +9,6 @@ host=localhost
 commands="CREATE DATABASE IF NOT EXISTS \`${newDb}\`;CREATE USER IF NOT EXISTS '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT ALL privileges ON \`${newDb}\`.*
 TO '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';FLUSH PRIVILEGES;"
 
+echo ${commands}
 mysql -u root -p -e "${commands}"
 cat bumon-test.sql | mysql -u ${newUser} -p${newDbPassword} ${newDb}
